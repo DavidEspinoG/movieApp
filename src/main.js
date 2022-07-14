ecf781ba7dc26311a53e0d9a64505778d3a96974
David Espino <davidespinog@gmail.com>
2022-07-13 22:00:29 -0500

    seccion  movie details

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    window.onresize = function(){
+        console.log(window.innerHeight)
+    }
+})
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
     headers: {
@@ -7,6 +12,25 @@ const api = axios.create({
         'api_key': API_KEY,
     }
 });
+
+const BASE_URL = 'https://api.themoviedb.org/3/'
+// async function getMovieInfo(movieId){
+//     // Conexion a base de datos
+//     const res = await fetch(BASE_URL + 'movie/' + movieId + '?api_key=' + API_KEY)
+//     const data = await res.json()
+//     const titledb = data.title
+//     console.log(titledb)
+//     // Crear HTML 
+//     const body = document.querySelector('body')
+//     const div = document.createElement('div')
+//     div.classList.add('movie-details-blur')
+//     body.appendChild(div)
+//     // body.classList.add('fijar-body')
+//     const title = document.createElement('h1')
+//     const titleText = document.createTextNode(data.title)
+//     title.appendChild(titleText)
+//     div.appendChild(title)
+// }
 // Fetch nativo
 async function getTrendingMoviesPreview(){
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
@@ -17,6 +41,9 @@ async function getTrendingMoviesPreview(){
         const apiImage = document.createElement('img')
         apiImage.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path)
         trendingContainerImg.appendChild(apiImage)
+        apiImage.onclick = function() {
+            getMovieInfo(movie.id)
+        }
     })
 }
 // Fetch con axios
