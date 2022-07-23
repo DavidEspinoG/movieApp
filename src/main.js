6f6eb33aa7ba7b2d82b7200fbe46edb0b140b5ab
David Espino <davidespinog@gmail.com>
2022-07-23 13:58:28 -0500

    movie details functionality

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function(){
     window.onresize = function(){
         console.log(window.innerHeight)
@@ -14,25 +15,9 @@ const api = axios.create({
 });
 
 const BASE_URL = 'https://api.themoviedb.org/3/'
-// async function getMovieInfo(movieId){
-//     // Conexion a base de datos
-//     const res = await fetch(BASE_URL + 'movie/' + movieId + '?api_key=' + API_KEY)
-//     const data = await res.json()
-//     const titledb = data.title
-//     console.log(titledb)
-//     // Crear HTML 
-//     const body = document.querySelector('body')
-//     const div = document.createElement('div')
-//     div.classList.add('movie-details-blur')
-//     body.appendChild(div)
-//     // body.classList.add('fijar-body')
-//     const title = document.createElement('h1')
-//     const titleText = document.createTextNode(data.title)
-//     title.appendChild(titleText)
-//     div.appendChild(title)
-// }
 // Fetch nativo
 async function getTrendingMoviesPreview(){
+    // Obtiene la información general de la película
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json()
     const movies = data.results
@@ -42,7 +27,7 @@ async function getTrendingMoviesPreview(){
         apiImage.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path)
         trendingContainerImg.appendChild(apiImage)
         apiImage.onclick = function() {
-            getMovieInfo(movie.id)
+            getMovieDetails(movie.id, movie.poster_path, movie.title, movie.overview)
         }
     })
 }
@@ -59,3 +44,69 @@ async function getCategoriesPreview(){
         categoryContainer.appendChild(categoryElement)
     })
 }
+// Obtener movie details
+async function getMovieDetails(id, poster_path, title, overview){
+    // Consulta a db
+    const res = await fetch(BASE_URL + 'movie/' + id + '?api_key=' + API_KEY);
+    const data = await res.json()
+    //añade blur al contenedor principal
+    const blur = document.getElementById('blur')
+    blur.classList.add('blur')
+    // Inyecta un div al body con la clase .movie-details-container
+    const body = document.querySelector('body')
+    const movieDetailsContainer = document.createElement('div')
+    movieDetailsContainer.classList.add('movie-details-container')
+    movieDetailsContainer.id = 'movie-details-container'
+    body.appendChild(movieDetailsContainer)
+    // Inyecta una imagen al movie details container
+    const img = document.createElement('img')
+    img.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + poster_path)
+    movieDetailsContainer.appendChild(img)
+    // Inyecta un div con la clase movie info 
+    const movieInfoDiv = document.createElement('div');
+    movieInfoDiv.classList.add('movie-info')
+    movieDetailsContainer.appendChild(movieInfoDiv)
+    // Añade título de la película
+    const htmlTitle = document.createElement('h2')
+    const textTitle = document.createTextNode(title)
+    htmlTitle.appendChild(textTitle)
+    movieInfoDiv.appendChild(htmlTitle);
+    // Añade un p con la overview de la pelicula
+    const htmlOverview = document.createElement('p')
+    const textOverview = document.createTextNode(overview)
+    htmlOverview.appendChild(textOverview)
+    movieInfoDiv.appendChild(htmlOverview)
+    // Añade el boton cerrar movie details 
+    const cerrarDiv = document.createElement('div')
+    cerrarDiv.classList.add('contenedor-cerrar')
+    movieInfoDiv.appendChild(cerrarDiv)
+    const botonCerrar = document.createElement('button')
+    botonCerrar.classList.add('btn-cerrar')
+    // botonCerrar.setAttribute('id', 'cerrar')
+    const textButton = document.createTextNode('X')
+    botonCerrar.appendChild(textButton)
+    cerrarDiv.appendChild(botonCerrar)
+
+    addListener()
+}
+// Cerrar movie details
+function addListener(){
+    const closeButtons = document.querySelectorAll('.btn-cerrar')
+closeButtons.forEach(closeButton => {
+    closeButton.addEventListener('click', closeDetails, false)
+})
+}
+const closeButtons = document.querySelectorAll('.btn-cerrar')
+closeButtons.forEach(closeButton => {
+    closeButton.addEventListener('click', closeDetails, false)
+})
+
+function closeDetails() {
+    const body = document.querySelector('body')
+    const contenedorDetails = document.getElementById('movie-details-container');
+    // contenedorDetails.classList.add('display-none')
+    body.removeChild(contenedorDetails)
+    const blur = document.getElementById('blur')
+    blur.classList.remove('blur')
+}
+addListener()
