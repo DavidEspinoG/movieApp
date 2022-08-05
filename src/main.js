92c1748c852d35a913d3400bc1fb36ea927b3f6a
David Espino <davidespinog@gmail.com>
2022-08-04 21:07:02 -0500

    responsive design

--- src/main.js
+++ src/main.js
@@ -16,7 +16,6 @@ async function getTrendingMoviesPreview(){
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json()
     const movies = data.results
-    console.log(movies)
     movies.forEach(movie => {
         const trendingContainerImg = document.getElementById('trending__imgs_container')
         const apiImage = document.createElement('img')
