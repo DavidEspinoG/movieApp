2fb4c51999696d494b33fdda9282d95674069ecb
David Espino <davidespinog@gmail.com>
2022-09-04 12:49:45 -0500

    loading skeleton trending movies

--- src/main.js
+++ src/main.js
@@ -16,8 +16,9 @@ async function getTrendingMoviesPreview(){
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json()
     const movies = data.results
-    movies.forEach(movie => {
-        const trendingContainerImg = document.getElementById('trending__imgs_container')
+    const trendingContainerImg = document.getElementById('trending__imgs_container')
+    trendingContainerImg.innerHTML = '';
+        movies.forEach(movie => {
         const apiImage = document.createElement('img')
         apiImage.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path)
         trendingContainerImg.appendChild(apiImage)
